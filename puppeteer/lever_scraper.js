d2ea1a2c5eacdd8c2e56a23131c1984b987b3c80
dlinch <dlinch33@gmail.com>
2018-10-16 23:44:51 -0600

    wrapped lever scraper in a try/catch block

--- puppeteer/lever_scraper.js
+++ puppeteer/lever_scraper.js
@@ -5,42 +5,46 @@ const json2csv = require('json2csv').parse;
 // json2csv documentation: https://www.npmjs.com/package/json2csv
 
 async function scrape(url) {
-  // Spin up browser instance
-  const browser = await puppeteer.launch({
-    headless: true,
-  });
+  try {
+    // Spin up browser instance
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
 
-  // Navigate to page
-  const page = await browser.newPage();
-  await page.goto(url);
-  // await screenshot(page, 'careers_page');
-  // await toPdf(page, 'careers_page');
+    // Navigate to page
+    const page = await browser.newPage();
+    await page.goto(url);
+    // await screenshot(page, 'careers_page');
+    // await toPdf(page, 'careers_page');
 
-  // Scrape data
-  let data = [];
-  const jobs = page.$$('section.Engineering li.job');
+    // Scrape data
+    let data = [];
+    const jobs = page.$$('section.Engineering li.job');
 
-  jobs.forEach(async (job) => {
-    await Promise.all([
-      await page.waitForNavigation(),
-      await job.click(),
-    ]);
-    await screenshot(page, 'job_page');
-    scrapedJob = {};
-    scrapedJob.title = await page.$('div.posting-headline h2').innerText;
-    scrapedJob.location = await page.$('div.posting-categories div.sort-by-time').innerText;
-    scrapedJob.commitment = await page.$('div.posting-categories div.sort-by-commitment').innerText;
-    data.push(scrapedJob);
-    await Promise.all([
-      await page.waitForNavigation(),
-      await page.goBack(),
-    ]);
-  });
+    jobs.forEach(async (job) => {
+      await Promise.all([
+        await page.waitForNavigation(),
+        await job.click(),
+      ]);
+      await screenshot(page, 'job_page');
+      scrapedJob = {};
+      scrapedJob.title = await page.$('div.posting-headline h2').innerText;
+      scrapedJob.location = await page.$('div.posting-categories div.sort-by-time').innerText;
+      scrapedJob.commitment = await page.$('div.posting-categories div.sort-by-commitment').innerText;
+      data.push(scrapedJob);
+      await Promise.all([
+        await page.waitForNavigation(),
+        await page.goBack(),
+      ]);
+    });
 
-  // Save data to csv
-  const csv = parseCSV(data);
-  write(csv);
-  await browser.close();
+    // Save data to csv
+    const csv = parseCSV(data);
+    write(csv);
+    await browser.close();
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 async function screenshot(page, name) {
